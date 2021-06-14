d5750dbad34971a49434d7cec935dc53275f398c
Guillaume <guillaumegozillon@gmail.com>
2021-06-14 18:55:22 +0200

    clickable timespan

--- src/components/Player.js
+++ src/components/Player.js
@@ -38,6 +38,11 @@ const Player = ({currentSong, isPlaying, setIsPlaying}) => {
         )
     }
 
+    const dragHandler = e => {
+        audioRef.current.currentTime = e.target.value
+        setsongInfo({...songInfo, currentTime: e.target.value})
+    }
+
     // State
     const [songInfo, setsongInfo] = useState({
         currentTime: null,
@@ -48,7 +53,12 @@ const Player = ({currentSong, isPlaying, setIsPlaying}) => {
         <div className="player">
             <div className="time-control">
                 <p>{getTime(songInfo.currentTime)}</p>
-                <input type="range" />
+                <input 
+                    min={0} 
+                    max={songInfo.duration} 
+                    value={songInfo.currentTime} 
+                    onChange={dragHandler}
+                    type="range" />
                 <p>{getTime(songInfo.duration)}</p>
             </div>
             <div className="play-control">
